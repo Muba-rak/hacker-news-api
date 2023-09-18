2d6eec3527c7ed59fec2345565b1c6cbb8cf3355
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-09-18 14:48:47 +0100

    set up pagination

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useGlobalContext } from "../context";
 
 const Buttons = () => {
-  return <div>Buttons</div>;
+  const { handlePage, isLoading, nbPages, page } = useGlobalContext();
+  return (
+    <div className="btn-container">
+      <button disabled={isLoading} onClick={() => handlePage("decrease")}>
+        Prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button disabled={isLoading} onClick={() => handlePage("increase")}>
+        Next
+      </button>
+    </div>
+  );
 };
 
 export default Buttons;
