c94d7317c4e07faba1686457ffd3bacf15cd2442
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-09-18 13:04:00 +0100

    created project with vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h2>Hacker news Starter</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
